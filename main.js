cf7b3589c45effa16c8131f8ca7984b5feac83b8
Ericraft009 <107516752+Ericraft009@users.noreply.github.com>
2022-11-29 10:19:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ menuListArray = ["Pizza Vegetariana",
                     "Pizza Extravaganza"];
 
 function getMenu(){
-var htmldata="";
-menuListArray.sort();
-for(var i=0;i<menuListArray.lenght;i++){
-    htmldata=htmldata+ menuListArray[i] + '<br>'
-    }
-    document.getElementById("displayMenu").innerHTML = htmldata;
+  var htmldata="";
+  menuListArray.sort();
+  for(var i=0;i<menuListArray.length;i++){
+      htmldata=htmldata+ menuListArray[i] + '<br>'
+  }
+  document.getElementById("displayMenu").innerHTML = htmldata;
 }
 
 
